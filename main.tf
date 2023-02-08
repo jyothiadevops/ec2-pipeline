e999fc10a4b611e85de8156a06bd76063e994545
Saishankar <82284490+jyothiadevops@users.noreply.github.com>
2023-02-08 20:12:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ec2" {
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  availability_zone = "${var.availability_zone}"
+  vpc_security_group_ids = "${var.security_groups}"
+  subnet_id             = "${var.subnet_id}"
+}
+  tags = {
+    Name = "${var.instance_name}"
+  }
