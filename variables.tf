48ac02c230142a0b1c2ca096508b23d4745a55b2
Saishankar <82284490+jyothiadevops@users.noreply.github.com>
2023-02-14 22:54:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 
-variable "instance_name" {
+variable "hostname" {
   description = "EC2 Hostname"
   default = "MyInstance"
 }
@@ -13,7 +13,13 @@ variable "region" {
 variable "ami" {
   description = "The EC2 AMI to use"
   type        = string
-  default     = "ami-03cf1a25c0360a382"
+  default     = "ami-0123456789abcdefg"
+}
+
+variable "count" {
+  description = "No of instances required"
+  type        = string
+  default     = "1"
 }
 
 variable "instance_type" {
@@ -22,26 +28,72 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "availability_zone" {
-  description = "The Availability Zone to launch resources in"
-  type        = string
-  default     = "us-east-1a"
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "security_groups" {
   description = "The list of security group IDs"
   type        = list(string)
-  default     = ["sg-0ed51816f400fad69"]
-}
-
-variable "key_name" {
-  description = "Key to login to the server"
-  type        = string
-  default     = "shared.nonprod"
+  default     = ["sg-0123456789abcdefg"]
 }
 
 variable "subnet_id" {
   description = "The Subnet ID to launch resources in"
   type        = string
-  default     = "subnet-03310e0f5b6c59870"
+  default     = "subnet-0123456789abcdefg"
+}
+variable "key_name" {
+  description = "Key to login to the server"
+  type        = string
+  default     = "shared.nonprod.pem"
+}
+
+// Tag details
+
+variable "SCTaskNo" {
+  description = "Service Now ticket Number"
+  type        = string
+  default     = "SCTASK0573291"
+}
+variable "Owner" {
+  description = "Owner of this server or team"
+  type        = string
+  default     = "GCS"
+}
+variable "support_group" {
+  description = "Who will support this server operations"
+  type        = string
+  default     = "support@cloudreach.com"
+}
+variable "Application" {
+  description = "Application running on this server"
+  type        = string
+  default     = "LRS/Custom"
+}
+variable "BU" {
+  description = "Bussiness Unit or Billing details"
+  type        = string
+  default     = "Custom"
+}
+variable "Maintinance_Window" {
+  description = "MW for patching or any downtime related activites"
+  type        = string
+  default     = "3rd Sunday of everymonth 10AM to 11AM"
+}
+variable "Environment" {
+  description = "Environment Details"
+  type        = string
+  default     = "Non-Prod"
+}
+variable "Automation" {
+  description = "Used for Automatic start/stop, Backup etc"
+  type        = string
+  default     = "yes"
+}
+variable "Backup" {
+  description = "Backup Window or policy "
+  type        = string
+  default     = "Non-prod-policy"
 }
