14b4cda08e53f4e68c7ec757912ece3d3332d297
Saishankar <82284490+jyothiadevops@users.noreply.github.com>
2023-02-14 22:54:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
 resource "aws_instance" "ec2" {
   ami           = "${var.ami}"
+  count = "${var.count}"
   instance_type = "${var.instance_type}"
-  availability_zone = "${var.availability_zone}"
+  availability_zone = var.azs[count.index]
   vpc_security_group_ids = "${var.security_groups}"
   subnet_id             = "${var.subnet_id}"
-  key_name                 = "${var.key_name}"
+  key                   = "${var.key_name}"
   tags = {
-    Name = "${var.instance_name}"
+    Name = "${var.hostname}"
+    SCTaskNo ="${var.SCTaskNo}"
+    Owner = "${var.owner_name}"
+    support_group = "${var.support_group}"
+    Application = "${var.Application}"
+    BU = "${var.BU}"
+    Maintinance_Window = "${var.Maintinance_Window}"
+    Environment = "${var.Environment}"
+    Automation = "${var.Automation}"
+    Backup = "${var.Backup}"
+    
   }
 }
